2563bd2e751d9072bd90ca7d3fe73d70a748919c
MarioDam <marioturan.mv@gmail.com>
2021-12-10 19:19:12 +0100

    subiendo cambios

--- app/src/main/java/com/example/gymario/MainActivity.java
+++ app/src/main/java/com/example/gymario/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.gymario;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private String nombreUsuario;
     private String password;
     private EditText usuario;
     private EditText passwd;
-    //Hola
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +28,15 @@ public class MainActivity extends AppCompatActivity {
     public void entrar(View view) {
         nombreUsuario = usuario.getText().toString();
         password = passwd.getText().toString();
+        if (usuario == null && passwd == null) {
+            Toast.makeText(this,"Introduce algo valor en las casillas", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,"Muy bien", Toast.LENGTH_SHORT).show();
+
+        }
         Intent i = new Intent(this, SegundaActividad.class);
-        i.putExtra("usuario",nombreUsuario);
-        i.putExtra("passwd",password);
+        i.putExtra("usuario", nombreUsuario);
+        i.putExtra("passwd", password);
         startActivity(i);
     }
 
--- app/src/main/java/com/example/gymario/SegundaActividad.java
+++ app/src/main/java/com/example/gymario/SegundaActividad.java
@@ -1,8 +1,6 @@
 package com.example.gymario;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
